4e967e1a91d8ba0d9cb406033998277111b0b9e4
Vinson Chuong <vinsonchuong@gmail.com>
2018-07-22 12:05:01 -0700

    feat(buildQuery): Add initial type annotations for JSON queries

--- adapters/build-query.js
+++ adapters/build-query.js
@@ -2,7 +2,31 @@
 import { branchOnClient } from 'rumor-mill/clients'
 import SQLBuilder from 'json-sql-builder2'
 
-type Query = {}
+type CreateIndex = {| $createIndex: {} |}
+type CreateTable = {| $createTable: {} |}
+type CreateView = {| $createView: {} |}
+type Delete = {| $delete: {} |}
+type Except = {| $except: {} |}
+type Insert = {| $insert: {} |}
+type Intersect = {| $intersect: {} |}
+type Select = {| $select: {} |}
+type Union = {| $union: {} |}
+type Update = {| $update: {} |}
+type With = {| $with: {} |}
+
+type Query =
+  | CreateIndex
+  | CreateTable
+  | CreateView
+  | Delete
+  | Except
+  | Insert
+  | Intersect
+  | Select
+  | Union
+  | Update
+  | With
+
 type ParsedQuery = {
   sql: string,
   values: Array<string | number | boolean | null>
